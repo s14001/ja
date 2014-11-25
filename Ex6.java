1c6f07a791a58876c7907bad94feba232820876a
s14001 <s14001@std.it-college.ac.jp>
2014-11-25 18:03:48 +0900

    Ex6終了

--- Ex6.java
+++ Ex6.java
@@ -1,16 +1,22 @@
 class Ex6{
 
 	public int [] ex6_1(){
-		
-		int [] ret = new int[1];
-		
+        int []    a={5,4,3,2,1};
+		int [] ret = new int[a.length];
+    for(int i=0;i<a.length;i++){
+      ret[i]=a[i];
+    }
+
 		return ret;
-	}
+    }
+
 
 	public double [] ex6_2(){
-		
-		double [] ret = new double[1];
-		
+        double []   a={1.1,2.2,3.3,4.4,5.5};
+		double [] ret = new double[a.length];
+     for(int i=0;i<a.length;i++){
+       ret[i]=a[i];
+    }
 		return ret;
 	}
 }
